e0be2ba63270eaf9c85a301239b068e7a931d2e9
Piero <mendozza1005@gmail.com>
2025-12-02 19:40:44 -0500

    commit

--- angular-tailwind/src/app/core/constants/menu.ts
+++ angular-tailwind/src/app/core/constants/menu.ts
@@ -8,62 +8,55 @@ export class Menu {
       items: [
         
           {
-          icon: 'assets/icons/heroicons/outline/building-office.svg',
+          icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Inmobiliarias',
           route: '/components/Listarinmobiliaria',
           protected: true, 
            
         },
         {
-          icon: 'assets/icons/heroicons/outline/user-group.svg',
+          icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Usuario',
-          route: '/components/table copy',
+          route: '/components/usuario',
         },
                 {
           icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Credito Prestamo',
-          route: '/components/credito-prestamo',
-           
-        },
-      ],
-    },
-    {
-      group: 'Collaboration',
-      separator: true,
-      items: [
-        {
-          icon: 'assets/icons/heroicons/outline/download.svg',
-          label: 'Download',
-          route: '/download',
+          route: '/components/credito-prestamo',          
         },
         {
-          icon: 'assets/icons/heroicons/outline/gift.svg',
-          label: 'Gift Card',
-          route: '/gift',
-        },
-        {
-          icon: 'assets/icons/heroicons/outline/users.svg',
+          icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Clientes',
           route: '/components/cliente',
 
         },
         {
-          icon: 'assets/icons/heroicons/outline/calculator.svg',
+          icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Simulador Financiero',
           route: '/components/registraeditacreditoprestamo',
 
         },
         {
-          icon: 'assets/icons/heroicons/outline/currency-dollar.svg',
+          icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Moneda',
           route: '/components/moneda',
         },
         {
-          icon: 'assets/icons/heroicons/outline/chart-bar.svg',
+          icon: 'assets/icons/heroicons/outline/cube.svg',
           label: 'Tasa de Interés',
           route: '/components/tasa-interes',
         },
+                {
+          icon: 'assets/icons/heroicons/outline/cube.svg',
+          label: 'Entidades Financieras',
+          route: '/components/entidad-financiera-table',
+        },        {
+          icon: 'assets/icons/heroicons/outline/cube.svg',
+          label: 'Entidad Tasa',
+          route: '/components/entidad-tasa-table',
+        },
       ],
     },
+    
   ];
 }
--- angular-tailwind/src/app/modules/uikit/uikit-routing.module.ts
+++ angular-tailwind/src/app/modules/uikit/uikit-routing.module.ts
@@ -8,6 +8,19 @@ import { CreditoPrestamoRegistrarComponent } from './pages/table/components/cred
 import { TableUsuarioComponent } from './pages/usuario/table.usuario.component';
 import { CronogramaFrancesComponent } from './pages/table/components/cronograma-metodo-frances/cronograma-metodo-frances.component';
 import { IndicadoresFrancesComponent } from './pages/table/components/indicadores-metodo-frances/indicadores-metodo-frances.component';
+import { TableClienteComponent } from './pages/cliente/table.cliente.component';
+import { TablePrestamoComponent } from './pages/creditoprestamo/table.prestamo.component';
+import { InmobiliariaComponent } from './pages/cronograma/components/inmobiliaria/inmobiliaria.component';
+import { ListarinmobiliariaComponent } from './pages/cronograma/components/listarinmobiliaria/listarinmobiliaria.component';
+import { RegistraeditausuarioComponent } from './pages/rol/components/registraeditausuario/registraeditausuario.component';
+import { RegistraeditaclienteComponent } from './pages/cliente/components/registraeditacliente/registraeditacliente.component';
+import { RegistraeditaprestamoComponent } from './pages/creditoprestamo/components/creaeditaprestamo/registraeditacredito.component';
+import { MonedaComponent } from './pages/cronograma/components/moneda/moneda.component';
+import { MonedaRegistrarComponent } from './pages/cronograma/components/moneda-registrar/moneda-registrar.component';
+import { TasaInteresComponent } from './pages/cronograma/components/tasa-interes/tasa-interes.component';
+import { TasaInteresRegistrarComponent } from './pages/cronograma/components/tasa-interes-registrar/tasa-interes-registrar.component';
+import { EntidadFinancieraTableComponent } from './pages/table/components/entidad-financiera-table/entidad-financiera-table.component';
+import { EntidadTasaTableComponent } from './pages/table/components/entidad-tasa-table/entidad-tasa-table.component';
 
 const routes: Routes = [
   {
@@ -16,9 +29,15 @@ const routes: Routes = [
     children: [ 
       { path: '', redirectTo: 'components', pathMatch: 'full' },
       { path: 'table', component: TableComponent },
+      { path: 'moneda', component: MonedaComponent },
+      { path: 'moneda-registrar', component: MonedaRegistrarComponent },
+       { path: 'tasa-interes', component: TasaInteresComponent },
+      { path: 'tasa-interes-registrar', component: TasaInteresRegistrarComponent },
        { path: 'usuario', component: TableUsuarioComponent },
        { path: 'cliente', component: TableClienteComponent },
        { path: 'creditoprestamo', component: TablePrestamoComponent },
+      { path: 'entidad-financiera-table', component: EntidadFinancieraTableComponent },
+      { path: 'entidad-tasa-table', component: EntidadTasaTableComponent },
       { path: 'metodo_frances', component: MetodoFrancesComponent },
       { path: 'inmobiliaria', component: InmobiliariaComponent },
       { path: 'Listarinmobiliaria', component: ListarinmobiliariaComponent },
